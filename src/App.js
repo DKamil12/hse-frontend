b3678134f480090c4c6613c0ef6db49e75c2db47
DKamil12 <dkamil381@gmail.com>
2022-07-15 12:23:22 +0600

    Fixed some bugs of different screen sizes content display.
    	modified:   src/App.js
    	modified:   src/assets/style/authorization_style.css
    	modified:   src/assets/style/doc_list_style.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // import Authorization from './Authorization';
 // import OrgStructure from './OrgStructure';
 // import DocFolders from './DocFolders';
-// import DocList from './DocList';
+import DocList from './DocList';
 import Navigation from './components/Navigation';
-import CreateDocForm from './CreateDocForm';
+// import CreateDocForm from './CreateDocForm';
 
 function App() {
   return (
     <div>
       <Navigation/>
-      <CreateDocForm/>
+      {/* <CreateDocForm/> */}
+      <DocList/>
     </div>
   );
 }
